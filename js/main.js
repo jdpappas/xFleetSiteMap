f2185b52f8a1ae866254d7fae6ae16eb91262a3d
Ioannis Pappas <jdpappas@outlook.com>
2014-08-03 15:42:59 +0200

    Create module and highlight the selected tab

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-function DetailsController ($scope) {
-    $scope.selectTab = function(selection) {
+var xFleetSiteMap = angular.module('xFleetSiteMap', []);
+
+xFleetSiteMap.controller('DetailsController', ['$scope', function($scope) {
+    // The mouse handler when clicking on the navigation panel.
+    $scope.selectTabHandler = function(selection) {
+        // If a previous selection is already made, then we have to reset it.
+        if ($scope.activeContent) 
+            $scope.activeContent.selected = false;
+        // Assign the current clicked link 
         $scope.activeContent = selection;
+        selection.selected = true; 
     };
-    $scope.detailSources = [{li:"Webpage Beschreibung", url:"content/description.html"},
-                            {li:"Externe JavaScript Dateien", url:"content/externalJSfiles.html"},
-                            {li:"Eingebettete JavaScript Dateien", url:"content/embeddedJSfiles.html"}];
-}
+   
+    // The links to the source files for the tab contents.
+    $scope.detailSources = [{li:"Webpage Beschreibung", url:"content/description.html", selected:false},
+                            {li:"Externe JavaScript Dateien", url:"content/externalJSfiles.html", selected:false},
+                            {li:"Eingebettete JavaScript Dateien", url:"content/embeddedJSfiles.html", selected:false}];
+}]);
 
