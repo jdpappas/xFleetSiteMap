2740f67ad8d456d0f3ab82315f5ec3df8674aaf0
jdpappas <jdpappas@outlook.com>
2014-08-05 10:22:21 +0200

    JSON contents and change of used JS files by clicking the proper submenu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var xFleetSiteMap = angular.module('xFleetSiteMap', []);
 
-xFleetSiteMap.controller('DetailsController', ['$scope', function($scope) {
+xFleetSiteMap.controller('MainController', ['$http', '$scope', '$log', function($http, $scope, $log) {
     // The mouse handler when clicking on the navigation panel.
     $scope.selectTabHandler = function(selection) {
         // If a previous selection is already made, then we have to reset it.
@@ -12,8 +12,34 @@ xFleetSiteMap.controller('DetailsController', ['$scope', function($scope) {
     };
    
     // The links to the source files for the tab contents.
-    $scope.detailSources = [{li:"Webpage Beschreibung", url:"content/description.html", selected:false},
-                            {li:"Externe JavaScript Dateien", url:"content/externalJSfiles.html", selected:false},
-                            {li:"Eingebettete JavaScript Dateien", url:"content/embeddedJSfiles.html", selected:false}];
+    $scope.detailSources = [{li:"Webpage Beschreibung", selected:false},
+                            {li:"Externe JavaScript Dateien", selected:false},
+                            {li:"Eingebettete JavaScript Dateien", selected:false}];
+
+
+    // JSON data are retrieved.
+    $http.get('/content/sitemap.json').success(function(data, status, headers, config) { 
+        $scope.sitemap = data; 
+        // An array is created with the proper layout (17x9) so that the upper table is properly displayed
+        var rows = [];
+        for (var l=0; l<17; l++) {
+            rows[l] = new Array(data.length);
+        }
+        // The submenu names are taken from the retrieved JSON data and assigned to the created array. 
+        for (var i=0; i<data.length; i++) {
+            for (var j=0; j<data[i].subMenus.length; j++) {
+                rows[j][i]=data[i].subMenus[j].submenuName;
+            }
+        }
+        $scope.tableRows = rows;
+        
+        // The method is called when a table from the top table is clicked and displays the corresponding JS files.
+        $scope.retrieveJSfiles = function(index1, index2){
+            if (data[index2].subMenus[index1]) 
+            $scope.javaScriptFiles = data[index2].subMenus[index1].requiredJS;
+        };
+    });
+    
 }]);
+
 
