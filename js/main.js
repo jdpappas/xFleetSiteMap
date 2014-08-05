e58a664968a91071df4ad41d1dcedc7ede70d7f6
jdpappas <jdpappas@outlook.com>
2014-08-05 12:13:21 +0200

    Change the definition of the sitemap CSS

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ xFleetSiteMap.controller('MainController', ['$http', '$scope', '$log', function(
         $scope.activeContent = selection;
         selection.selected = true; 
     };
-   
+       
     // The links to the source files for the tab contents.
     $scope.detailSources = [{li:"Webpage Beschreibung", selected:false},
                             {li:"Externe JavaScript Dateien", selected:false},
@@ -20,23 +20,12 @@ xFleetSiteMap.controller('MainController', ['$http', '$scope', '$log', function(
     // JSON data are retrieved.
     $http.get('/content/sitemap.json').success(function(data, status, headers, config) { 
         $scope.sitemap = data; 
-        // An array is created with the proper layout (17x9) so that the upper table is properly displayed
-        var rows = [];
-        for (var l=0; l<17; l++) {
-            rows[l] = new Array(data.length);
-        }
-        // The submenu names are taken from the retrieved JSON data and assigned to the created array. 
-        for (var i=0; i<data.length; i++) {
-            for (var j=0; j<data[i].subMenus.length; j++) {
-                rows[j][i]=data[i].subMenus[j].submenuName;
-            }
-        }
-        $scope.tableRows = rows;
         
         // The method is called when a table from the top table is clicked and displays the corresponding JS files.
-        $scope.retrieveJSfiles = function(index1, index2){
-            if (data[index2].subMenus[index1]) 
-            $scope.javaScriptFiles = data[index2].subMenus[index1].requiredJS;
+        $scope.retrieveJSfiles = function(subMenu){
+            //if (data[index2].subMenus[index1]) 
+            $scope.javaScriptFiles = subMenu.requiredJS;
+            $log.log(subMenu.requiredJS);
         };
     });
     
